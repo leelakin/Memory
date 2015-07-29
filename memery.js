3fe54bf194968988228b4b2c3a3567a236e284b4
leelakin <fen@leelakin.com>
2015-07-29 23:29:21 +0100

    turn allImgs into proper Array before addEventListener
    with Array.prototype.slice.call(allImgs)

--- memery.js
+++ memery.js
@@ -1,19 +1,19 @@
 
 window.onload = function(){
-	var allImgs = document.getElementsByClassName("pft"); // THIS RETURNS AN ARRAY WITH ALL ELEMENTS W/THAT CLASS
-	for (var i=0; i<allImgs.length; i++){
-		allImgs[i].addEventListener("mouseover", flipOver); // ADDS THE EVENT LISTENER TO EVERY SINGLE IMAGE BY LOOPING THROUGH ARRAY
-		//COULD ALSO USE THE MAP FUNCTION FOR THIS! --> running a function on each array element:
-		/*allImgs.map( function(arrayCells){
-				arrayCells.addEventListener("click, flipOver")
-			}
-			); */
-	}; 
-	//no brackets after function var because it would get called immediately
-}
+	var allImgs = document.getElementsByClassName("pft"); // THIS RETURNS A HTML COLLECTION WITH ALL ELEMENTS W/THAT CLASS
+	var newImgs = [].slice.call(allImgs); //call(allImgs) makes call function think it's being called from allImgs instead of the array.
+	console.log(newImgs);
+	newImgs.map(function(arrayCell){
+		console.log(arrayCell);
+		arrayCell.addEventListener("mouseover", flipOver);
+	}
+	);
+}; 
+	//no brackets after function var because it's just a reference, not a call
+
 
 function flipOver(event){
-	
+
 	var memes = document.getElementById("mems");
 	if (event.target.src.match("2x2a")){
 
@@ -38,5 +38,5 @@ function flipOver(event){
 // check whether any of the other cells are NOT face down (loop through allImgs[i].src.match != 2x2a etc). If no, put "uncover another card" in a p.
 //   if yes, check whether the cell's img src matches that of any of the other cells (loop through allImgs)
 // 
-// --> if there's a match, the 2 matching cards should get taken out of the array but also stop them from flipping (removeEventListener?)
-//     (or...? document.getElementById("imgXyz").className = "";  <-- no class at all left, but it gets HTML collection on window load so prob too late)
+// --> if there's a match, the 2 matching cards should get taken out of the array but also stop them from flipping (HOW? disable js code on these? Exclude)
+// document.getElementById("imgXyz").className = "";  <-- no class at all left!
