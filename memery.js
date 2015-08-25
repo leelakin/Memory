bd5125ecdaf993abda1321fd924b92ed2c4c9edb
leelakin <fen@leelakin.com>
2015-08-25 17:10:30 +0100

    Finished functionality
    Pure JavaScript, no jQuery or stylesheets yet

--- memery.js
+++ memery.js
@@ -1,32 +1,87 @@
 
 window.onload = function(){
-	var allImgs = document.getElementsByClassName("pft"); // THIS RETURNS A HTML COLLECTION WITH ALL ELEMENTS W/THAT CLASS
-	var newImgs = [].slice.call(allImgs); //call(allImgs) makes call function think it's being called from allImgs instead of the array.
-	console.log(newImgs);
-	newImgs.map(function(arrayCell){
-		console.log(arrayCell);
-		arrayCell.addEventListener("mouseover", flipOver);
-	}
+	
+	allImgs().map(function(arrayCell){
+		arrayCell.addEventListener("click", matchMaking); //no brackets after function var because it's just a reference, not a call
+		}
 	);
 }; 
-	//no brackets after function var because it's just a reference, not a call
 
 
-function flipOver(event){
+function allImgs(){
+	var allImgs = document.getElementsByClassName("pft"); // THIS RETURNS A HTML COLLECTION WITH ALL ELEMENTS W/THAT CLASS
+	return [].slice.call(allImgs); //call(allImgs) makes call function think it's being called from allImgs instead of the array.
+}//I need the [array] to add the EventListener via .map(). Might be faster to just loop thru HTML collection in window.onload to addEventListener.
 
-	var memes = document.getElementById("mems");
-	if (event.target.src.match("2x2a")){
+var imgArray = function(){
+	return [3,1,6,4,2,3,5,2,4,5,6,1] //Set order for now; there are 12 cards, so 6 image pairs.
+}; 
 
-		event.target.src = "img/trololo.jpg";
-		memes.innerHTML = "MEMERY";
-		memes.style.color = "red";
+var activatedCards = []; //is there a way to initialise this non-globally? Would I have to put it in another function?
+var countedPairs = 0;
+
+var activate = function(turnedId,turnedSrc){ //can't just hand over event.target and then ask for id & src within function because the scope has changed
+	activatedCards.push({position: turnedId, pic: turnedSrc}); 
+	return activatedCards;//all elements that have been clicked (max. 2) as objects in a array
+};
+
+var pairCounter = function(){
+	if (countedPairs == 5){
+		document.getElementById("lilhelp").innerHTML ="YOU MADE IT YAAAAAY&emsp;<button id=\"newgame\">New game</button>";
+		document.getElementById("newgame").onclick = function(){window.location.reload()};
+	};
+	return countedPairs++;
+};
+
+function matchMaking(event){  //EVERY TIME A CARD GETS CLICKED, ALL OF THIS HAPPENS
+//"event" hands over the mouseevent; event.target is the element that triggered the event.
+
+	if (event.target.src.match("back")){
+
+		event.target.src = "img/img"+imgArray()[event.target.id-1]+".png"// CARD FLIPS (change to img url grabbed from array above, ideally random assignment)
+		
+		activate(event.target.id, event.target.src); //puts element as object into activeCards
+
+		//now check whether this open card matches another one by checking the length of the "active cards" array.
+		//if the length is 2, check both objects' "picture" properties in the "active cards" array.
+		if (activatedCards.length == 2){ //2 cards have been uncovered!
+
+			if (activatedCards[activatedCards.length-1].pic === activatedCards[activatedCards.length-2].pic){ //if they match
+
+				document.getElementById("lilhelp").innerHTML = "YOU FOUND A PAIR OMG";
+				pairCounter(); //counting up number of pairs
+				activatedCards = []
+
+			} else { // put all of the else content in a function and setTimeOut ??
+				document.getElementById("lilhelp").innerHTML = "Nooope."; //if they don't match
+				
+				// get the position value from each object in the array
+				// then change the dom elements' src with this id
+				setTimeout(function(){
+
+				activatedCards[0];
+				activatedCards[1];
+
+				document.getElementById(activatedCards[0].position).src = "img/back.png";
+				document.getElementById(activatedCards[1].position).src = "img/back.png";
+				document.getElementById("lilhelp").innerHTML = "Try again, bruh";
+				activatedCards = []
+
+			},700)
+			}
+		} else if (activatedCards.length == 1){
+			document.getElementById("lilhelp").innerHTML = "Uncover another card."
+		} else {
+			console.log("this shouldn't ever be the case (nothing in object array after an item has been clicked)");
+		}
 
 	} else {
 
-		event.target.src = "img/2x2a.png";
-		memes.innerHTML = "MEMORY";
-		memes.style.color = "black";
+		document.getElementById("lilhelp").innerHTML ="This card is already turned over, what are you playing at"
+
 	}
+	
+	return activatedCards;
 };
 
 
